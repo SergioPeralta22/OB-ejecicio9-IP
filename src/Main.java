a6c1502fa253b73bf4d002e000f3ea2a12ebd6a5
Sergio Peralta <seperalta88@gmail.com>
2022-06-19 01:17:27 -0300

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cliente cliente = new Cliente();
+        cliente.edad = 20;
+        cliente.nombre = "Juan";
+        cliente.telefono = 123456789;
+        cliente.credito = 5000;
+
+        System.out.println("Nombre: " + cliente.nombre);
+        System.out.println("Edad: " + cliente.edad);
+        System.out.println("Telefono: " + cliente.telefono);
+        System.out.println("Credito: " + cliente.credito);
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.edad = 30;
+        trabajador.nombre = "Pedro";
+        trabajador.telefono = 987654321;
+        trabajador.salario = 1000;
+
+        System.out.println("Nombre: " + trabajador.nombre);
+        System.out.println("Edad: " + trabajador.edad);
+        System.out.println("Telefono: " + trabajador.telefono);
+        System.out.println("Salario: " + trabajador.salario);
+
     }
+}
+
+class Persona {
+    int edad;
+    String nombre;
+    int telefono;
+}
+
+class Cliente extends Persona {
+    int credito;
+}
+
+class Trabajador extends Persona {
+    int salario;
 }
